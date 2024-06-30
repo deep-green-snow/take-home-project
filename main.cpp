2bb79ef624f3680e795450da0579a8b5f00fc75b
deep-green-snow <dldudvy777@gmail.com>
2024-06-30 16:13:59 +0900

    first commit

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+
+// card
+// - number (16 digits)
+// - pin (6 digits)
+// - connected account
+
+// account
+// - number (13 digits)
+// - balance 
+// 
+
+
+
+
+int main()
+{
+    cout << "hello, world" << endl;
+    return 0;
+}
